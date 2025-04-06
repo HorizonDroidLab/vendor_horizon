507e0cb39c869e0572df87ef3ab68fd932beae93
Rve27 <rve27github@gmail.com>
2025-04-06 18:24:49 +0800

    config: Build GameSpace
    Signed-off-by: Rve27 <rve27github@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -86,6 +86,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/base/data/keyboards/Vendor_045e_Product_028e.kl:$(TARGET_COPY_OUT_PRODUCT)/usr/keylayout/Vendor_045e_Product_0719.kl
 
+# GameSpace
+PRODUCT_PACKAGES += \
+    GameSpace
+
 # Face Unlock
 TARGET_FACE_UNLOCK_SUPPORTED ?= $(TARGET_SUPPORTS_64_BIT_APPS)
 
